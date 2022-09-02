ccdf17f1f84b43f6cdcb9b418d1ad47a66e9ec84
saira512dev <512saira@gmail.com>
2022-09-02 19:00:14 +0530

    Completed Goals apis

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3000
+const colors = require('colors')
+const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
