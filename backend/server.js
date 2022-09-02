bc08d3e6084a34b1119e061f48a5ba19c47c2809
saira512dev <512saira@gmail.com>
2022-09-02 17:02:24 +0530

    Goals controller and routes set

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 3000
+const { errorHandler } = require('./middleware/errorMiddleware')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, console.log(`connected on port ${PORT}`))
